3e114de0e92f77b44eac6e07f832bab416a35260
Parktaehoon1 <92parkth@gmail.com>
2022-08-25 11:23:44 +0900

    프로젝트생성

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <BlogHeader style="text-align:center"/>
+  <BlogInput />
+  <BlogList />
+  <BlogFooter/>
+</template>
+
+<script>
+  import BlogHeader from "@/components/BlogHeader.vue";
+  import BlogInput from "@/components/BlogInput.vue";
+  import BlogList from "@/components/BlogList.vue";
+  import BlogFooter from "@/components/BlogFooter.vue";
+export default {
+    components:{
+      BlogHeader,
+      BlogInput,
+      BlogList,
+      BlogFooter
+    },
+    setup(){
+
+    return{
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>

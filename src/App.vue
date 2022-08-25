e0ed5d163a167aa633e1287fd71f72cd7f22f430
Parktaehoon1 <92parkth@gmail.com>
2022-08-25 11:47:49 +0900

    html 초기화 및 구조화 작업

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <template>
-  <BlogHeader style="text-align:center"/>
-  <BlogInput />
-  <BlogList />
-  <BlogFooter/>
+  <div class="wrap">
+    <BlogHeader />
+    <BlogInput />
+    <BlogList />
+    <BlogFooter />
+  </div>
 </template>
 
 <script>
@@ -10,21 +12,56 @@
   import BlogInput from "@/components/BlogInput.vue";
   import BlogList from "@/components/BlogList.vue";
   import BlogFooter from "@/components/BlogFooter.vue";
-export default {
-    components:{
+  export default {
+    components: {
       BlogHeader,
       BlogInput,
       BlogList,
       BlogFooter
     },
-    setup(){
+    setup() {
 
-    return{
+      return {}
     }
   }
-}
 </script>
 
 <style>
+  @charset 'utf-8';
+  @import url('https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;300;400;500;700;900&family=Roboto:wght@100;300;400;500;700;900&display=swap');
 
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  ul,
+  li {
+    list-style: none;
+  }
+
+  img {
+    vertical-align: middle;
+    border: 0;
+  }
+
+  a {
+    color: #333;
+    text-decoration: none;
+  }
+
+  html {
+    font-size: 16px;
+    overflow-x: hidden;
+  }
+
+  body {
+    font-family: 'Roboto', 'Noto Sans KR', sans-serif;
+    font-size: 14px;
+    font-weight: 400;
+    line-height: 1.2;
+    letter-spacing: -0.64;
+    color: #000;
+  }
 </style>

e5e90d17bbaa89f3460810847d2d418a15485c76
Parktaehoon1 <92parkth@gmail.com>
2022-08-25 13:14:44 +0900

    input 란 공백 경고 진행

--- src/components/BlogInput.vue
+++ src/components/BlogInput.vue
@@ -1,38 +1,104 @@
 <template>
-	<div class="input-wrap">
-		<input type="text" v-model="newItem">
-		<button v-on:click="addItem">추가</button>
+	<div class="input-wrap shadow">
+		<input type="text" v-model="newItem" class="input-box" maxlength="30" @keyup.enter="addItem">
+		<!-- <button v-on:click="addItem">추가</button> -->
+		<span @click="addItem" class="add-bt">
+			<i class="fas fa-plus add-bt-icon"></i>
+		</span>
 	</div>
 </template>
 
 <script>
-	import { ref } from 'vue';
+	import {
+		ref
+	} from 'vue';
 
 	export default {
 		setup() {
 			const newItem = ref('');
 			const addItem = () => {
 				let temp = newItem.value;
-				//localStorage.setItem(키, 값)
-				//localStorage.setItem(키, json 문자열로 저장)
-				// json 저장 문자열은
+				temp = temp.trim();
+
+				if (temp !== '') {
+					//localStorage.setItem(키, 값)
+					//localStorage.setItem(키, json 문자열로 저장)
+					// json 저장 문자열은
+					/*
+						{
+							completed: false, 
+							title: '메모내용', 
+							icon: 파일명 
+						}
+					*/
+					localStorage.setItem(temp, temp);
+					resetItem();
+				} else {
+					alert('공백쳤음');
+				}
 				/*
-					{
-						completed: false, 
-						title: '메모내용', 
-						icon: 파일명 
-					}
+				let reg = /\s/g;
+				let checkNewItem = newItem.value.replace(/\s/g, "");
+				if(newItem.value === reg && checkNewItem){
+					localStorage.setItem(temp, temp)
+						alert('공백아님')
+					} else {
+					alert('공백치지마세여')
+				}
+				newItem.value = '';
 				*/
-				localStorage.setItem(temp, temp)
-				newItem.value= '';
+			}
+			// 내용 재설정
+			const resetItem = () => {
+				newItem.value = '';
 			}
 			return {
-				newItem,addItem
+				newItem,
+				addItem
 			}
 		}
 	}
 </script>
 
-<style>
+<style scoped>
+.input-wrap{
+	position: relative;
+	display: block;
+	height: 50px;
+	line-height: 50px;
+	border-radius: 5px;
+	background-color: #fff;
+	overflow: hidden;
+
+	margin: 20px 0;
+}
+.input-wrap input{
+	border-style: none;
+}
+.input-wrap input:focus{
+	outline: none;
+}
+.input-box{
+	width: 70%;
+	margin-left: 20px;
+	font-size: 16px;
+	font-weight: 700;
+}
+
+
+.add-bt{
+	display: block;
+	float: right;
+	background-color: lightgray;
+	cursor: pointer;
+
+}
+
+.add-bt-icon{
+	display: inline-block;
+	vertical-align: middle;
+	color: #fff;
+	margin: 0 20px;
+}
 
 </style>
